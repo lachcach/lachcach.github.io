aadab58eb6afc9e72f7dc39a050d46e98808a8d4
ducanh1041 <69205118+ducanh1041@users.noreply.github.com>
2021-03-26 11:34:27 +0700

    Initial commit

---
+++ js/process.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {progressIndicator()};
+
+function progressIndicator() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
